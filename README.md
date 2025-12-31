4e404f5ac9bb9c967693d5560637a18ba638c824
LAzyEagleIsHere <lymivan1110@hotmail.com>
2025-12-31 22:17:57 +0800

    zooooooooom

--- README.md
+++
@@ -1,2 +0,0 @@
-# World-Transport-Free
-WTF
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import random
 import time
 from pygame.math import Vector2
+from text import *
 from HK import *
 
 pygame.init()
@@ -20,6 +21,9 @@ gray = (200, 200, 200)
 orange = (255, 165, 0)
 light_blue = (173, 116, 233)
 
+# --- Rails Colour
+tko_line_colour = (100, 0, 100)
+
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('W-T-F')
 font = pygame.font.Font(None, 36)
@@ -28,7 +32,7 @@ font = pygame.font.Font(None, 36)
 station_circle_radius = 15
 
 
-# --- TKO line ---
+# --- TKO Line ---
 
 tko_line_stations_name = {
   "Po Lam", "Hang Hau", "TKO", "LOHAS Park", "TKL", "Yau Tong", "Quarry Bay", "North Point"
@@ -45,44 +49,91 @@ tko_line_stations_pos = {
   "North Point": (300, 300)
 }
 
-tko_line_stations_branched = {
+# tko_line_stations_branches = {
+#   "Po Lam": ["Hang Hau"], 
+#   "Hang Hau": ["Po Lam"], 
+#   "TKO": ["Hang Hau", "LOHAS Park", "TKL"], 
+#   "LOHAS Park": ["TKO"], 
+#   "TKL": ["TKO", "Yau Tong"], 
+#   "Yau Tong": ["TKL", "Quarry Bay"], 
+#   "Quarry Bay": ["Yau Tong", "North Point"], 
+#   "North Point": ["Quarry Bay"]
+# }
+
+# linked with stations
+tko_line_stations_branches = {
   "Po Lam": ["Hang Hau"], 
-  "Hang Hau": ["Po Lam"], 
-  "TKO": ["Hang Hau", "LOHAS Park", "TKL"], 
-  "LOHAS Park": ["TKO"], 
-  "TKL": ["TKO", "Yau Tong"], 
-  "Yau Tong": ["TKL", "Quarry Bay"], 
-  "Quarry Bay": ["Yau Tong", "North Point"], 
-  "North Point": ["Quarry Bay"]
+  "Hang Hau": ["TKO"], 
+  "TKO": ["LOHAS Park", "TKL"], 
+  "LOHAS Park": [], 
+  "TKL": ["Yau Tong"], 
+  "Yau Tong": ["Quarry Bay"], 
+  "Quarry Bay": ["North Point"], 
+  "North Point": []
 }
 
-tko_line_stations_opened = {
-  "Po Lam": True, 
-  "Hang Hau": False, 
-  "TKO": False, 
-  "LOHAS Park": False, 
-  "TKL": False, 
-  "Yau Tong": False,
-  "Quarry Bay": False, 
-  "North Point": False
+tko_line_stations_opened = {name: True for name in tko_line_stations_name}
+
+# tko_line_stations_opened = {
+#   "Po Lam": True, 
+#   "Hang Hau": False, 
+#   "TKO": False, 
+#   "LOHAS Park": False, 
+#   "TKL": False, 
+#   "Yau Tong": False,
+#   "Quarry Bay": False, 
+#   "North Point": False
+# }
+
+
+# --- KT Line ---
+
+kt_line_stations_branches = {
+  
 }
 
+kt_line_stations_opened = {
+  "TKL", "Yau Tong", 
+  "Lam Tin", "KT", 
+  "Ngau Tau Kok", "Kowloon Bay", 
+  "Choi Hung", "Diamond Hill", 
+  "Wong Tai Sin", "Lok Fu", 
+  "Kowloon Tong", "Shek Kip Mei", 
+  "Prince Edward", "Mong Kok", 
+  "Yau Ma Tie", "Ho Man Tin", 
+  "Whampoa"
+}
 
-# --- KT line ---
+# --- East Rail Line ---
 
 
-# --- East Rail line ---
+# --- Camera Settings
 
+zoom = 1.0
+offset_x, offset_y = 0, 0
+dragging = False
+last_mouse_pos = (0, 0)
 
+def transform(pos):
+  # zooming and offset
+  x, y = pos
+  return int(x * zoom + offset_x), int(y * zoom + offset_y)
 
 def main():
+  global zoom, offset_x, offset_y, dragging, last_mouse_pos
   running = True
   while running:
     screen.fill(black)
     
     for station_name in (tko_line_stations_name):
       if tko_line_stations_opened[station_name]:
-        pygame.draw.circle(screen, white, tko_line_stations_pos[station_name], station_circle_radius)
+        for branch in (tko_line_stations_branches[station_name]):
+          pygame.draw.line(screen, tko_line_colour, transform(tko_line_stations_pos[station_name]), transform(tko_line_stations_pos[branch]), 5)
+          # pygame.draw.circle(screen, gray, tko_line_stations_pos[branch], station_circle_radius)
+        pygame.draw.circle(screen, white, transform(tko_line_stations_pos[station_name]), int(station_circle_radius * zoom))
+    
+    
+    zoom_in_button = pygame.Rect(10, 10, 50, 50)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -92,7 +143,56 @@ def main():
           if event.key == pygame.K_ESCAPE:
             pygame.quit()
             sys.exit()
+        
+          # --- Zooming (Keyboard) ---
+          elif event.key == pygame.K_EQUALS or event.key == pygame.K_PLUS:
+            zoom *= 1.1
+          elif event.key == pygame.K_MINUS:
+            zoom /= 1.1
+          
+          # --- Screen Movements (Keyboard) ---
+          elif event.key == pygame.K_LEFT:
+            offset_x += 20
+          elif event.key == pygame.K_RIGHT:
+            offset_x -= 20
+          elif event.key == pygame.K_UP:
+            offset_y += 20
+          elif event.key == pygame.K_DOWN:
+            offset_y -= 20
+        
+        # --- Zooming (Mouse) ---
+        
+        # Mouse Wheel
+        elif event.type == pygame.MOUSEWHEEL:
+          if event.y > 0: # scroll up
+            zoom *= 1.1
+          elif event.y < 0: # scroll down
+            zoom /= 1.1
+
+        # --- Screen Movements (Mouse) ---
+        
+        # Mouse Drag
+        # start
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+          if event.button == 1: # left click
+            dragging = True
+            last_mouse_pos = event.pos
+        
+        # stop
+        elif event.type == pygame.MOUSEBUTTONUP:
+          if event.button == 1:
+            dragging = False
+        
+        # Screen Move While Dragging
+        elif event.type == pygame.MOUSEMOTION:
+          if dragging:
+            dx = event.pos[0] - last_mouse_pos[0]
+            dy = event.pos[1] - last_mouse_pos[1]
+            offset_x += dx
+            offset_y += dy
+            last_mouse_pos = event.pos        
   
     pygame.display.flip()
+    clock.tick(fps)
   
 main()
