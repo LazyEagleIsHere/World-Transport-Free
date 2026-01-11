920e258344a984e172cb3c094ff18765f010f8b4
LAzyEagleIsHere <lymivan1110@hotmail.com>
2026-01-11 20:56:41 +0800

    Red Pao Series

--- trains.py
+++ trains.py
@@ -2,24 +2,35 @@ train_types = {
   "Steamer", 
   "Polar", 
   "Rickshaw", 
+  
+  # Physics Series
   "Magnetic", 
   "Magnetic Heavy", 
   "Quantum", 
   "Photon", 
-  "Red Pao", 
+  
+  # Red Pao Series
+  "Small Red Pao", 
+  "Medium Red Pao", 
+  "Big Red Pao", 
+  "Gigantic Red Pao", 
+  
   "Mushroom Bouncer", 
   "Mosquito Heavy", 
 }
 
 trains_info = {
-  "Steamer": {"fuel" : 1500, "engines": 1, "fuel car": 1, "speed": 60, "jobs": 8, "parts": 4}, 
-  "Polar": {"fuel": 1700, "engines": 1, "fuel car": 2, "speed": 70, "jobs": 8, "parts": 6}, 
+  "Steamer": {"fuel" : 1000, "engines": 1, "fuel car": 1, "speed": 60, "jobs": 8, "parts": 4}, 
+  "Polar": {"fuel": 1500, "engines": 1, "fuel car": 2, "speed": 70, "jobs": 8, "parts": 6}, 
   "Rickshaw": {"fuel": 10000000000000000000000, "engines": 1, "fuel car": 5, "speed": 1, "jobs": 30, "parts": 1}, 
   "Magnetic": {"fuel": 2500, "engines": 2, "fuel car": 2, "speed": 140, "jobs": 12, "parts": 6}, 
   "Magnetic Heavy": {"fuel": 3000, "engines": 3, "fuel car": 3, "speed": 170, "jobs": 16, "parts": 10}, 
   "Quantum": {"fuel": 4000, "engines": 1, "fuel car": 1, "speed": 250, "jobs": 20, "parts": 15}, 
   "Photon": {"fuel": 4000, "engines": 1, "fuel car": 2, "speed": 250, "jobs": 15, "parts": 15}, 
-  "Red Pao": {"fuel": 2000, "engines": 2, "fuel car": 3, "speed": 200, "jobs": 14, "parts": 12}, 
+  "Small Red Pao": {"fuel": 1500, "engines": 1, "fuel car": 1, "speed": 150, "jobs": 10, "parts": 12}, 
+  "Medium Red Pao": {"fuel": 1750, "engines": 1, "fuel car": 2, "speed": 160, "jobs": 10, "parts": 12}, 
+  "Big Red Pao": {"fuel": 1750, "engines": 2, "fuel car": 1, "speed": 160, "jobs": 10, "parts": 12}, 
+  "Gigantic Red Pao": {"fuel": 2000, "engines": 2, "fuel car": 2, "speed": 200, "jobs": 10, "parts": 12}, 
   "Mushroom Bouncer": {"fuel": 2500, "engines": 2, "fuel car": 1, "speed": 160, "jobs": 13, "parts": 5}, 
   "Mosquito Heavy": {"fuel": 5000, "engines": 1, "fuel car": 3, "speed": 160, "jobs": 20, "parts": 13}, 
 }
@@ -28,10 +39,13 @@ normal_trains = {
   "Steamer", 
   "Polar", 
   "Magnetic", 
-  "Red Pao", 
+  "Small Red Pao", 
+  "Medium Red Pao", 
+  "Big Red Pao", 
 }
 
 special_trains = {
+  "Gigantic Red Pao", 
   "Magnetic Heavy", 
   "Quantum", 
   "Photon", 
